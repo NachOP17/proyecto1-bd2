ac89b5535f327ff221da7a216761cea9b54668b8
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-05-06 12:56:31 -0400

    Se agregó el filtro del tiempo

--- server.js
+++ server.js
@@ -15,41 +15,65 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post('/search', (req, res) => {
+app.get('/search', (req, res) => {
   after = "";
   dist = 0;
-  getPosts(res)
+  console.log(req.query);
+  let jsonRes = req;
+  let query = jsonRes.query;
+  let text = query.texto;
+  let time = query.tiempo;
+  (async () => {
+    text = text.toLowerCase();
+    text = encodeURI(text);
+    getPosts(text, time);
+    var check = () => {
+      console.warn('checking')
+      if(after == null)
+        res.render('index.hbs', {
+          dist: dist
+        })
+      else
+        setTimeout(check, 100)
+    }
+    setTimeout(check, 100)
+  })()
 })
 
-function getPosts(response) {
+
+
+function getPosts(text, time) {
+  console.log(text)
+  console.log(time)
+  var endpoint = 'https://www.reddit.com/search.json?q=' + text + '&limit=100&t=' + time;
   console.log("Se ejecuto")
   if (after == null) {
-    response.render('index.hbs', {
-      dist: dist
-    });
+    return null
   } else if (after == "") {
-    Request.get(`https://www.reddit.com/search.json?q=hello&t=day`, (error, response, body) => {
+    Request.get(endpoint, (error, response, body) => {
       if (error) {
         return console.log(error);
       }
       let res = JSON.parse(body);
       after = res.data.after;
+      console.log(res.data.dist)
       dist = dist + Number(res.data.dist);
       console.log(after);
       console.log(dist);
-      getPosts(response)
+      getPosts(text, time)
     });
   } else {
-    Request.get(`https://www.reddit.com/search.json?q=hello&t=day&after=${after}`, (error, response, body) => {
+    Request.get(`https://www.reddit.com/search.json?q=${text}&t=${time}limit=100&after=${after}`, (error, response, body) => {
       if (error) {
         return console.log(error);
       }
       let res = JSON.parse(body);
       after = res.data.after;
+      console.log(res.data.dist)
       dist = dist + Number(res.data.dist);
       console.log(after);
       console.log(dist);
-      getPosts(response)
+      getPosts(text, time)
     });
   }
 }
